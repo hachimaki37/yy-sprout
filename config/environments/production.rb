963d57c54a395f6e5bb4b3de361408d07afb5007
Yutaka Yamaki <yutaka.yamaki@mrtaddy.com>
2020-11-19 11:09:45 +0900

    fix production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,11 +91,11 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.action_mailer.default_url_options   = { host: 'yy-sprout.herokuapp.com' }
+  host = 'yy-sprout.herokuapp.com'
+  config.action_mailer.default_url_options   = { host: host }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
+  ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
     :domain => 'herokuapp.com',
