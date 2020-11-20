091665040351d32b67c1995ca080daea2c415aa3
Yutaka Yamaki <yutaka.yamaki@mrtaddy.com>
2020-11-20 11:10:35 +0900

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,18 +68,17 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-  host = 'yy-sprout.herokuapp.com'
+  config.action_mailer.delivery_method = :smtp
+  host = 'https://yy-sprout.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
-  config.action_mailer.smtp_settings = {
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    domain: 'herokuapp.com',
-    address: 'smtp.sendgrid.net',
-    port: 587,
-    authentication: :plain,
-    enable_starttls_auto: true
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => host,
+    :authentication => :plain,
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
