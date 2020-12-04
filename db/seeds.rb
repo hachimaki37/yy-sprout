fe927984f7111737d41004272c0bcc44076ee35d
Yutaka Yamaki <yutaka.yamaki@mrtaddy.com>
2020-12-04 17:03:28 +0900

    add record

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(nickname: "スプロート",
+            email: "sproutfc@example.jp",
+            password:  "sprout12345",
+            password_confirmation: "sprout12345",
+            admin: true)
