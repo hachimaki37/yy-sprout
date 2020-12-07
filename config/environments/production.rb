422dd8af5cddad3744a1c4d243e9de4529531721
Yutaka Yamaki <yutaka.yamaki@mrtaddy.com>
2020-12-07 23:31:57 +0900

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,5 +103,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  GA.tracker = ENV['GA_TRACKER']
+  GA.tracker = "G-5ZXW6KT070"
 end
