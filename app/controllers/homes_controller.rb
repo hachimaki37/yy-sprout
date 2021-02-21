7edbc6f801c52cfbe62a3639501866e235843ce0
YAMAKI YUTAKA <yutakayamaki@YAMAKInoMacBook-Air.local>
2021-02-22 00:03:12 +0900

    fix files

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,8 +2,4 @@ class HomesController < Match::ScheduleResultsController
   def index
     @schedule_result = public_method(:index).super_method.call.where(match_result: nil).first
   end
-  def
-    'herrol'
-  end
-
 end
